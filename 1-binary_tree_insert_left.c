6b74cd502cb9bf0ce09e2430fe015f90c31763d1
Nicolas <nico15935746@gmail.com>
2021-03-09 01:00:02 -0500

    Parent == null

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -18,7 +18,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 
 	new = malloc(sizeof(binary_tree_t));
 
-	if (new == NULL)
+	if (new == NULL || parent == NULL)
 		return (NULL);
 
 	new->n = value;
