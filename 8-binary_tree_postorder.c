3f06d74ff68eded59660279ae083bfc79ee2990e
Nicolas <nico15935746@gmail.com>
2021-03-09 01:47:03 -0500

    Same as 6 to reverse

---
+++ 8-binary_tree_postorder.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+#include <stdlib.h>
+
+/**
+ * binary_tree_postorder - goes through a binary tree using pre-order traversal.
+ *
+ * Args:
+ *      @tree: node to tree to liberate.
+ *      @func: function to print
+ *
+ */
+
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (!tree || !func)
+		return;
+
+	binary_tree_preorder(tree->left, func);
+	binary_tree_preorder(tree->right, func);
+	func(tree->n);
+}
