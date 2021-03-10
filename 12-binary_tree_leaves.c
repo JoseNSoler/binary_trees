c6828e608dba98298dcf810e8d66cf107fd9e496
Nicolas <nico15935746@gmail.com>
2021-03-10 01:55:47 -0500

    Check all leaves

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -12,14 +12,14 @@
 
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
+	size_t leaves = 0;
+
 	if (!tree)
 		return (0);
 
-	if (tree->left && tree->right)
-		return (2);
-	if (tree->left || tree->right)
+	if (!tree->left && !tree->right)
 		return (1);
-	if (!tree->parent && !tree->right && !tree->left)
-		return (0);
-	return (1);
+	leaves += binary_tree_leaves(tree->left);
+	leaves += binary_tree_leaves(tree->right);
+	return (leaves);
 }
