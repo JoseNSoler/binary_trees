bc3374568b81d7159a8552cde07071f7783615f1
Nicolas <nico15935746@gmail.com>
2021-03-10 01:41:59 -0500

    [Updated] Check if parent exists

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -17,5 +17,9 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 
 	if (tree->left && tree->right)
 		return (2);
-	return (1);
+	if (tree->left || tree->right)
+		return (1);
+	if (tree->parent)
+		return (1);
+	return (0);
 }
