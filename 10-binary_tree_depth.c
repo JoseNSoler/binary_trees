73428d12b9dca1fdd1d29a183d1b3f1c03200621
Nicolas <nico15935746@gmail.com>
2021-03-10 00:53:22 -0500

    Check the depth of a ndoe

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,23 @@
+#include "binary_trees.h"
+#include <stdlib.h>
+
+/**
+ * binary_tree_depth - Check the depth of a binary tree.
+ * @tree: node to tree.
+ * Return: 0 if tree is NULL, other num = depth of the node.
+ */
+
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	size_t dep = 0;
+
+	if (!tree)
+		return (0);
+
+	while (tree->parent)
+	{
+		dep +=1;
+		tree = tree->parent;
+	}
+	return dep;
+}
