b5690ecab70e4d89b4553421901a382e8bb06ebd
Nicolas <nico15935746@gmail.com>
2021-03-11 00:07:24 -0500

    Run both sides a plus 1

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -1,24 +1,17 @@
 #include "binary_trees.h"
 
 /**
-* binary_tree_height - function that measures the height of a binary tree
+* binary_tree_nodes - Count nodes with at least 1 child in the tree
 * @tree: pointer to start of tree.
-* Return: 2 on two leafs, 1 in one leaf on any side,  and 0 on NULL
+* Return: 0 if tree NULL.
 */
 
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-	size_t childs;
-
-	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
+	if (tree == NULL)
 		return (0);
 
-	if (tree->right)
-		childs = binary_tree_nodes(tree->right) + 1;
-	if (tree->left)
-		childs = binary_tree_nodes(tree->left) + 1;
-	if (tree->parent == NULL)
-		childs += 1;
-
-	return (childs);
+	if (tree->left || tree->right)
+		return (binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right) + 1);
+	return (0);
 }
