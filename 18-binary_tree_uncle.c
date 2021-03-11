2f0f45b17fc5d4b4d3d32199b740532a7fcb5b85
Nicolas <nico15935746@gmail.com>
2021-03-11 01:22:10 -0500

    [File added] Check if the uncle exists

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -18,15 +18,19 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 	if (node->parent->parent)
 	{
 		if (node->parent->left == node)
+		{
 			if (node->parent->parent->left)
 				return (node->parent->parent->left);
 			else
 				return (NULL);
+		}
 		if (node->parent->right == node)
+		{
 			if (node->parent->parent->right)
 				return (node->parent->parent->right);
 			else
 				return (NULL);
+		}
 	}
 	return (NULL);
 }
