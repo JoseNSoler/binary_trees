f62a0afbadae1c312856692824532274045065da
Nicolas <nico15935746@gmail.com>
2021-03-11 00:29:01 -0500

    [Updated] Only check for height

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -36,10 +36,6 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 {
 	if (binary_tree_height(tree->left) != binary_tree_height(tree->right))
 		return (0);
-	if (!tree->left || !tree->right)
-		return (0);
 
-	binary_tree_is_perfect(tree->left);
-	binary_tree_is_perfect(tree->right);
 	return (1);
 }
