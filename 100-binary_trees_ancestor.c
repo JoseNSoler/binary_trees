2e99f51d90898c11a9eb8fb9ca6043d2524da916
Nicolas <nico15935746@gmail.com>
2021-03-11 15:47:14 -0500

    [Updated] Betty checks passed

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -1,30 +1,31 @@
 #include "binary_trees.h"
 
 /**
-* binary_tree_height - function that finds the lowest common ancestor of two nodes
+* binary_tree_ancestor - finds the lowest common ancestor of two nodes
 * @first: pointer to first node.
 * @second: pointer to second node.
 *
 * Return: LCA in common.
 */
 
-binary_tree_t *binary_trees_ancestor(const binary_tree_t * first,
-        const binary_tree_t * second) {
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+				     const binary_tree_t *second)
+{
+	binary_tree_t *First_parent, *Second_parent;
 
-    binary_tree_t *First_parent, *Second_parent;
+	if (!first || !second)
+		return (NULL);
+	if (first == second)
+		return ((binary_tree_t *)first);
 
-    if (!first || !second)
-        return (NULL);
-    if (first == second)
-        return ((binary_tree_t *)first);
+	First_parent = first->parent;
+	Second_parent = second->parent;
 
-    First_parent = first->parent;
-    Second_parent = second->parent;
-
-    if (first == Second_parent || !First_parent || (!First_parent->parent && Second_parent))
-        return (binary_trees_ancestor(first, Second_parent));
-    else if (First_parent == Second_parent || !Second_parent || (!Second_parent->parent && First_parent))
-        return (binary_trees_ancestor(First_parent, second));
-    return (binary_trees_ancestor(First_parent, Second_parent));
-
+	if (first == Second_parent || !First_parent ||
+	    (!First_parent->parent && Second_parent))
+		return (binary_trees_ancestor(first, Second_parent));
+	else if (First_parent == Second_parent || !Second_parent ||
+		 (!Second_parent->parent && First_parent))
+		return (binary_trees_ancestor(First_parent, second));
+	return (binary_trees_ancestor(First_parent, Second_parent));
 }
